cb174fe8e934c66c34ef6c834c4ba820d24a4d90
Tsekourakos <george.tsekouras@swisscom.com>
2021-04-13 17:45:42 +0200

    First implementation

---
+++ DemoBankService/src/main/java/com/checkstep/DemoBankService/DemoBankServiceApplication.java
@@ -0,0 +1,10 @@
+package com.checkstep.DemoShopService;
+
+@SpringBootApplication
+public class DemoBankServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
