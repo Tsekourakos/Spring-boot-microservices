29c81979deb7e1401220f8c047382498e6f6840e
Tsekourakos <34190036+Tsekourakos@users.noreply.github.com>
2021-04-15 11:19:05 +0200

    Restructure the project by using maven instead of gradle.

--- DemoBankService/src/main/java/com/checkstep/DemoBankService/DemoBankServiceApplication.java
+++ DemoBankService/src/main/java/com/checkstep/DemoBankService/DemoBankServiceApplication.java
@@ -1,10 +1,13 @@
-package com.checkstep.DemoShopService;
+package com.checkstep.DemoBankService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoBankServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserServiceApplication.class, args);
+        SpringApplication.run(DemoBankServiceApplication.class, args);
     }
 
 }
--- DemoShopService/src/main/java/com/checkstep/DemoShopService/DemoShopServiceApplication.java
+++ DemoShopService/src/main/java/com/checkstep/DemoShopService/DemoShopServiceApplication.java
@@ -1,10 +1,13 @@
 package com.checkstep.DemoShopService;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class DemoShopServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserServiceApplication.class, args);
+        SpringApplication.run(DemoShopServiceApplication.class, args);
     }
 
 }
